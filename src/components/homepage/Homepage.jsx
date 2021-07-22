efec5705148a74b3f2cc7964b281a070a54655b8
samuelomondi22 <omondisam696@gmail.com>
2021-07-22 08:32:19 -0600

    heaader

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,16 +1,13 @@
-// import { useLocation } from "react-router";
 import Header from "../header/Header";
-// import Posts from "../../components/posts/Posts";
+import Posts from "../../components/posts/Posts";
 import "./homepage.css";
 
 export default function Homepage() {
-  // const location = useLocation();
-  // console.log(location);
   return (
     <>
       <Header />
       <div className="home">
-        {/* <Posts /> */}
+        <Posts />
       </div>
     </>
   );
---
+++ src/components/post/Post1.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import "./post.css";
+import DateTime from '../date/DateTime.js';
+
+export default function Post({img}) {
+  return (
+    <div className="post">
+      <img
+        className="postImg"
+        src={img}
+        alt=""
+      />
+      <div className="postInfo">
+        <div className="postCats">
+        </div>
+        <span className="postTitle">
+          <Link to="/post/day1" className="link">
+            The Journey Begins.....
+          </Link>
+        </span>
+        <hr />
+      </div>
+      <p className="postDesc">
+        Experience the first day of the trip with us. Just follow the link above.....
+      </p>
+    </div>
+  );
+}

16f5e4a38917814dc6242bb9018555dde70ba849
samuelomondi22 <omondisam696@gmail.com>
2021-07-24 20:49:21 -0600

    day four

--- src/components/singlepost/SinglePost3.jsx
+++ src/components/singlepost/SinglePost3.jsx
@@ -3,6 +3,18 @@ import OldF from '../../images/oldf.jpg';
 import BabaSB from '../../images/babastevebrian.jpg';
 import Brian from '../../images/brian.jpg';
 import MudP from '../../images/mudpot.jpg';
+import Brian1 from '../../images/brian2.jpg';
+import BBS from '../../images/BBS.jpg';
+import Sam from '../../images/sam.jpg';
+import SmallG from '../../images/smallgeyser.jpg';
+import Brian3 from '../../images/brian3.jpg';
+import Familia3 from '../../images/familia3.jpg';
+import Brian4 from '../../images/brian4.jpg';
+import Teton from '../../images/teton.jpg';
+import Buffalo from '../../images/buffalos.jpg';
+import Fish from '../../images/fish.jpg';
+import Sam1 from '../../images/sam1.jpg';
+
 
 export default function SinglePost() {
   return (
@@ -37,14 +49,51 @@ export default function SinglePost() {
             <img className="singleImg" src={MudP} alt="Mud Pot"/>
             <br/>
             <br/>
+            <img className="singleImg" src={Brian3} alt="Brian"/>
+            <br/>
+            <br/>
+            <img className="singleImg" src={Brian4} alt="Brian"/>
+            <br/>
+            <br/>
             At Old Faithful, we were able to see about a 130 feet geyser. A geyser is a hot spring in which water boils,sending a tall water and steam into the air. They tend to be about 105-184 feet in height.
             Normally there is a crack on the surface of the earth and as water goes through the cracks, it makes its way down till it contacts hot rocks. 
             As the water boils up more water goes through the crack and now the boiling water can't escape so it builds pressure. When the pressure of the boiling water
             gets higher it pushes push the water on top of it out of its way. When this happens the water underneath shoots up making a good site to view.
             The water that was pushed out trickle into the cracks repeating the process again.
             <br/>
+             <br/>
+            <img className="singleImg" src={Brian1} alt="Brian"/>
+            <br/>
+            <br/>
+            <img className="singleImg" src={BBS} alt="Baba Steve"/>
+            <br/>
+            <br/>
+            <img className="singleImg" src={Sam} alt="Samuel"/>
+            <br/>
+            <br/>
+            <img className="singleImg" src={SmallG} alt="Brian"/>
+            <br/>
+            <br/>
+            <img className="singleImg" src={Familia3} alt="Familia"/>
+            <br/>
+            <br/>
+            <img className="singleImg" src={Sam1} alt="Samuel"/>
+            <br/>
+            <br/>
             At the evening we toured a fish hatchery. A fish hatchery is a place for artificial breeding, hatching and rearing of fish. We saw eye eggs - fertilized egg with the eye 
             of the fish starting to show, and some fish that were 13 months old.
+            <br/>
+            <br/>
+            <img className="singleImg" src={Fish} alt="Fish Hatchery"/>
+            <br/>
+            Fish Hatchery Spots
+            <br/>
+            <img className="singleImg" src={Buffalo} alt="Buffalos"/>
+            <br/>
+            <br/>
+            <img className="singleImg" src={Teton} alt="Grand Tetons"/>
+            <br/>
+            That is the Grand Tetons
         </p>
       </div>
     </div>

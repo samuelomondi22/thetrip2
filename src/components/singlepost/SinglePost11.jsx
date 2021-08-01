ac93af2fdc715ce0c85955f8677a2fed894f1f18
samuelomondi22 <omondisam696@gmail.com>
2021-07-31 20:11:10 -0600

    day eleven

--- src/components/singlepost/SinglePost11.jsx
+++ src/components/singlepost/SinglePost11.jsx
@@ -17,7 +17,22 @@ export default function SinglePost() {
           <span>July 30, 2021</span>
         </div>
         <p className="singlePostDesc">             
-           Coming Out Today!
+          We didn’t notice the room we were staying at had two air conditioners, so only one was turned on. 
+          But in the middle of the night we figured it out. A cooler night after that!
+          <br/>
+          We ate leftover Mexican food for breakfast. But Brian was feeling poorly since the previous night. 
+          So we discussed whether we wanted to see the homeless people (aka “bums”) in San Francisco, 
+          and see some more trees, or just go home. It was unanimous so we turned north and started driving. 
+          <br/>
+          It looked like it would be about 1,000 miles, and luckily we had started early. 
+          Brian was able to lay down across the bench seat in the back and get some rest. 
+          Samuel kept Steve awake. It was 106 degrees in Redding when we said goodbye to In-N-Out burgers and immediately we 
+          crossed the border to Oregon the temperature dropped to about 76 degrees followed by rain. We seem to take the rain with us as we travel. 
+          Not sorry to leave the heat, though. 
+          <br/>
+          Pretty good traffic and a nice sunset. 
+          Steve was driving a bit crazy (he was pretty tired) but we didn’t do any damage to the truck. 
+          And 15 hours after we started we pulled into our driveway. A long day! 
         </p>
       </div>
     </div>
